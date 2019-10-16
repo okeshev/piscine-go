787eb3ef1f250850028eaa52a6eaf8c1e0d10764
okeshev <okeshev@github.com>
2019-10-16 21:06:59 +0600

    commit

---
+++ printalphabet/main.go
@@ -0,0 +1,8 @@
+package main
+	import "github.com/01-edu/z01"
+		func main() {
+				for iRune := 'a'; iRune<= 'z'; iRune++ {
+										z01.PrintRune(iRune)
+									}
+				z01.PrintRune('\n')
+			}
