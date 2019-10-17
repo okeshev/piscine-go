592cbfcdad36337e19f029456f4cb36459328656
okeshev <okeshev@github.com>
2019-10-17 21:30:53 +0600

    ultimatedivmod.go

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,5 +1,14 @@
 package piscine
+import "fmt"
 
+
+func main() {
+	a := 13
+	b := 2
+	UltimateDivMod(&a, &b)
+	fmt.Println(a)
+	fmt.Println(b)
+}
 func UltimateDivMod(a *int, b *int) {
 	m := *a / *b
 	n := *a % *b
