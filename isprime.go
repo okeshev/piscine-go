079003480568823f74b214853ddc9653123d6a4f
okeshev <okeshev@github.com>
2019-10-21 23:01:31 +0600

    isprime.go

---
+++ isprime.go
@@ -0,0 +1,20 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	if nb < 0 || nb == 1 {
+		return false
+	} else if nb == 2 || nb == 3 {
+		return true
+	} else {
+		if nb%2 == 0{
+			return false
+		} else {
+			for i := 3; i <nb; i = i + 1 {
+				if nb%i == 0{
+					return false
+				}
+			}
+			return true
+		}
+	}
+}
