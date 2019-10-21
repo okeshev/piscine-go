1180da48bfef5d9edce19932f904f34aae63fa96
okeshev <okeshev@github.com>
2019-10-21 23:02:16 +0600

    isprime.go

--- isprime.go
+++ isprime.go
@@ -6,11 +6,11 @@ func IsPrime(nb int) bool {
 	} else if nb == 2 || nb == 3 {
 		return true
 	} else {
-		if nb%2 == 0{
+		if nb%2 == 0 {
 			return false
 		} else {
 			for i := 3; i <nb; i = i + 1 {
-				if nb%i == 0{
+				if nb%i == 0 {
 					return false
 				}
 			}
