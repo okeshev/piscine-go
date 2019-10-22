fa4e9a6bf52298b9d1a4d85cceff8a8240be383a
okeshev <okeshev@github.com>
2019-10-22 22:28:54 +0600

    alphacount.go

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(str string) int {
 	count := 0
 	for _, r := range str {
-		if (r >= 'A' && r <= 'z') {
+		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') {
 			count++
 		}
 	}
