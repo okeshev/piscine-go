59870000f1b495571852076fca0ce5562a30ada9
okeshev <okeshev@github.com>
2019-10-23 21:51:57 +0600

    toupper.go

---
+++ toupper.go
@@ -0,0 +1,14 @@
+package piscine
+
+}
+func ToUpper(s string) string {
+	res := ""
+	for _, r := range s {
+		if r >= 'a' && r <= 'z' {
+			res = res + string(r + ('A' - 'a'))
+		} else {
+			res = res + string(r)
+		}
+	}
+	return res
+}
