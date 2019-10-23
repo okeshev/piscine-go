42e591644361a5ca8946aabbaee911ebaefdc7c3
okeshev <okeshev@github.com>
2019-10-23 22:05:37 +0600

    isalpha.go

---
+++ isalpha.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsAlpha(str string) bool {
+	for _, r := range str {
+		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') {
+		} else {
+			return false
+		}
+	}
+	return true
+}
