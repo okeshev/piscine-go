1775c504db9667d3f6da33d4f60678c4b6e491cd
okeshev <okeshev@github.com>
2019-10-23 21:55:29 +0600

    toupper.go

--- toupper.go
+++ toupper.go
@@ -4,7 +4,7 @@ func ToUpper(s string) string {
 	res := ""
 	for _, r := range s {
 		if r >= 'a' && r <= 'z' {
-			res = res + string(r +('A'-'a'))
+			res = res + string(r+('A'-'a'))
 		} else {
 			res = res + string(r)
 		}
