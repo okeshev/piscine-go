167d43751cd80d3a06397fbfb8bc612d74dc89b8
okeshev <okeshev@github.com>
2019-10-24 20:06:26 +0600

    isprintable.go

---
+++ isprintable.go
@@ -0,0 +1,12 @@
+
+Learn more or give us feedback
+package piscine
+
+func IsPrintable(str string) bool {
+	for _, r := range str {
+		if r >= 0 && r <= 31 {
+			return false
+		}
+	}
+	return true
+}
